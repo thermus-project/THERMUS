c290137ef188b62e32af6ab1e283907333a8b99a
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2024-10-22 15:22:52 +0200

    Testing more

---
+++ test/all_predictions.C
@@ -0,0 +1,12 @@
+// Loop through all sets
+
+const TString[] Setlist=[
+    "PartList_PPB2002.txt",
+    "PartList_PPB2014.txt",
+    "PartList_PPB2014_CBHN.txt",
+    "PartList_PPB2014_CBHN_fixed_saveQM14.txt",
+    "PartList_PPB2018_CBHN.txt"];
+
+void all_predictions() {
+    
+}
