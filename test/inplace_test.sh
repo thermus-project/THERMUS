4b8993f86654020faa33b029cfeac2c753bee866
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2024-10-23 14:51:31 +0200

    Allowing testing on a list of files

--- test/inplace_test.sh
+++ test/inplace_test.sh
@@ -8,15 +8,16 @@ TESTDIR=`dirname -- "$( readlink -f -- "$0"; )"`
 
 # $THERMUS is not yet defined
 # So the argument is single-quoted to avoid expansion
-$RUN_THERMUS '$THERMUS/share/doc/Thermus/tests/prediction.C -b -q' > brut_result.txt
+$RUN_THERMUS '$THERMUS/share/doc/Thermus/tests/all_predictions.C -b -q'
 
-sed -n '/predicted values/,$p' brut_result.txt > result.txt
-
-diff result.txt $TESTDIR/prediction_expected_model_1.txt
-
-if [ $? -ne 0 ]; then
-  echo "TEST FAILED"
-  exit 1
-fi
+# For each file in the result directory, test if it's same as in the expected directory
+for ff in `ls $TESTDIR/results/*.txt`; do
+  f=`basename $ff`
+  diff $TESTDIR/results/$f $TESTDIR/expected_results/$f
+  if [ $? -ne 0 ]; then
+    echo "TEST FAILED for file $f"
+    exit 1
+  fi
+done
 
 echo "Test passed"
