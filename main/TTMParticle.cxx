e425a7fb06e71f7fc0e03cbdffc09b4a385d5736
Boris Hippolyte <boris.hippolyte@iphc.cnrs.fr>
2024-10-24 18:13:24 +0200

    adding back the file preamble

--- main/TTMParticle.cxx
+++ main/TTMParticle.cxx
@@ -1,3 +1,18 @@
+/************************************************************************
+ * Copyright(c) 2004-2018, THERMUS Project,        All rights reserved. *
+ *                                                                      *
+ * Author: The THERMUS Project (A Thermal Model Package for ROOT).      *
+ * Contributors (UCT-IPHC) are mentioned in the code where appropriate. *
+ *                                                                      *
+ * Permission to use, copy, modify and distribute this software and its *
+ * documentation strictly for non-commercial purposes is hereby granted *
+ * without fee, provided that the above copyright notice appears in all *
+ * copies and that both the copyright notice and this permission notice *
+ * appear in the supporting documentation. The authors make no claims   *
+ * about the suitability of this software for any purpose. It is        *
+ * provided "as is" without express or implied warranty.                *
+ ************************************************************************/
+
 // Author: Spencer Wheaton 26 April 2010 //
 //__________________________________________________________________________
 // Stores particle properties relevant to the thermal model.
