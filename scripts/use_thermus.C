c185e94cac9df7baa361ef8f3f40036f1d330196
yannick.patois@iphc.cnrs.fr <Yannick Patois>
2025-11-28 10:18:07 +0100

    Better layout

--- scripts/use_thermus.C
+++ scripts/use_thermus.C
@@ -1,9 +1,13 @@
 // Run this code to include Thermus in your ROOT session
+R__ADD_INCLUDE_PATH($THERMUS/include/Thermus)
+R__ADD_LIBRARY_PATH($THERMUS_LIB)
+R__LOAD_LIBRARY(libFunctions)
+R__LOAD_LIBRARY(libTHERMUS)
+
+TString THERMUS = gSystem->Getenv("THERMUS");
+TString THERMUS_LIB = gSystem->Getenv("THERMUS_LIB");
+
+void use_thermus()
 {
-  TString THERMUS=gSystem->Getenv("THERMUS");
-  TString THERMUS_LIB=gSystem->Getenv("THERMUS_LIB");
-  gSystem->AddIncludePath("-I"+THERMUS+"/includes/thermus");
-  gSystem->Load(THERMUS_LIB+"/libFunctions.so");
-  gSystem->Load(THERMUS_LIB+"/libTHERMUS.so");
-  std::cout << " *** Info: Root setup: \t THERMUS is included ( " << THERMUS << " )\t ***\n";
+   std::cout << " *** Info: Root setup: \t THERMUS is included ( " << THERMUS << " )\t ***\n";
 }
